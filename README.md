88371bee31e05d2ac5ae21d85270bc372d70be0c
Known_Black_Hat <knownblackhat@gmail.com>
2023-01-07 17:34:51 +0530

    Project Completed

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Parking-reservation-API
+
+
+> API for booking
+```bash
+curl 127.0.0.1:8080/book/<BAY NO.(1-4)>/<NAME>/<DATE(DD:MM:YYYY)>
+```
+
+> API for for admin to watch all bookings
+```bash
+curl 127.0.0.1:8080/dev
+```
+> API data structure: { DATE: {  SPOT: { "name" : NAME }  }  }
+
+## Examples
+
+- Alice, Bob, Charlie and Dave book a parking bay for 1st June 2022
+```bash
+curl 127.0.0.1:8080/book/1/Alice/01:06:2022
+ # Output: Congrats! Alice, your reserved bay no. 1 on 01/06/2022
+
+curl 127.0.0.1:8080/book/2/Bob/01:06:2022
+ # Output: Congrats! Bob, your reserved bay no. 2 on 01/06/2022
+
+curl 127.0.0.1:8080/book/3/Charlie/01:06:2022
+ # Output: Congrats! Charlie, your reserved bay no. 3 on 01/06/2022
+
+curl 127.0.0.1:8080/book/4/Dave/01:06:2022
+ # Output: Congrats! Dave, your reserved bay no. 4 on 01/06/2022
+```
+
+- Ed tried to book a parking bay on the same day but is unable to because there are no free days.
+```bash
+curl 127.0.0.1:8080/book/4/Ed/01:06:2022 
+# Output: Ed on 01:06:2022 bay 4 is already booked by someone 
+```
+
+- Ed books a parking bay for 2nd June 2022
+```bash
+curl 127.0.0.1:8080/book/1/Ed/02:06:2022
+# Output: Congrats! Ed, your reserved bay no. 1 on 02/06/2022 
+```
+
+- Querying bookings for 1st June 2022 returns 4 bookings with the allocated bay and each customer’s details.
+```bash
+curl 127.0.0.1:8080/dev/01:06:2022
+# Output: {"1":{"name":"Alice"},"2":{"name":"Bob"},"3":{"name":"Charlie"},"4":{"name":"Dave"}}
--- api.py
+++ api.py
@@ -1,62 +1,49 @@
 from flask import Flask,request
 import json 
-
+import datetime
 app = Flask(__name__)
 
 BAYS=4
 RESERVATIONS={}
 
-def rebook(spot,name,date):
+def rebook(name,date):
 	for i in range(1,BAYS):
 		if RESERVATIONS[date].get(i) and RESERVATIONS[date][i]["name"]==name:
 			return True
 	return False
-		
-
-
-def check_bays(spot,name,date):
-	if spot > BAYS:
-		return f"{name}, this spot {spot} doesn't exist"
-	elif len(date) < 8:
-		return "Invalid Date Format. Format Should be DD:MM:YYYY"
-	elif date[2] != ":" and date[5] != ":" :
-		return "Invalid Date Format. Format Should be DD:MM:YYYY"
-	elif RESERVATIONS.get(date) and RESERVATIONS[date].get(spot) and RESERVATIONS[date][spot]:
-		return f"{name} on {date} spot {spot} is already booked by someone"
-	elif RESERVATIONS.get(date) and rebook(spot,name,date):
-		return f"{name}, you already booked"
-	else:
-		if RESERVATIONS.get(date) is None:
-			RESERVATIONS[date]={}
-		RESERVATIONS[date][spot]={}
-		RESERVATIONS[date][spot]["name"]=name
-		return f"Congrats! {name}, your reserved spot no. {spot} for {date.replace(':','/')}"
-
 
 
 # Routing
 
-@app.route("/book/<int:spot>/<name>/<date>")
-def book(spot,name,date): #date -> DD:MM:YYYY
-	if spot > BAYS:
-		return f"{name}, this spot {spot} doesn't exist"
-	elif len(date) <= 8:
+@app.route("/book/<int:bay>/<name>/<date>")
+def book(bay,name,date): #date -> DD:MM:YYYY
+	try:
+		parsed_date = datetime.datetime.strptime(date,"%d:%m:%Y")
+	except ValueError:
 		return "Invalid Date Format. Format Should be DD:MM:YYYY"
-	elif date[2] != ":" or date[5] != ":" :
-		return "Invalid Date Format. Format Should be DD:MM:YYYY"
-	elif RESERVATIONS.get(date) and RESERVATIONS[date].get(spot) and RESERVATIONS[date][spot]:
-		return f"{name} on {date} spot {spot} is already booked by someone"
-	elif RESERVATIONS.get(date) and rebook(spot,name,date):
+	if bay > BAYS:
+		return f"{name}, this bay {bay} doesn't exist"
+	elif datetime.datetime.strptime(date,"%d:%m:%Y") == datetime.datetime.now().strftime('%d/%m/%Y'):
+		return f"{name}, Reservation can't be done on same day"
+	elif RESERVATIONS.get(date) and RESERVATIONS[date].get(bay) and RESERVATIONS[date][bay]:
+		return f"{name} on {date} bay {bay} is already booked by someone"
+	elif RESERVATIONS.get(date) and rebook(name,date):
 		return f"{name}, you already booked"
 	else:
 		if RESERVATIONS.get(date) is None:
 			RESERVATIONS[date]={}
-		RESERVATIONS[date][spot]={}
-		RESERVATIONS[date][spot]["name"]=name
-		return f"Congrats! {name}, your reserved spot no. {spot} for {date.replace(':','/')}"
+		RESERVATIONS[date][bay]={}
+		RESERVATIONS[date][bay]["name"]=name
+		return f"Congrats! {name}, your reserved bay no. {bay} on {parsed_date.strftime('%d/%m/%Y')}"
 
-@app.route("/dev")
-def dev():
-	return RESERVATIONS
+@app.route("/dev/<date>")
+def dev(date):
+	try:
+		datetime.datetime.strptime(date,"%d:%m:%Y")
+	except ValueError:
+		return "Invalid Date Format. Format Should be DD:MM:YYYY"
+	if RESERVATIONS.get(date):
+		return RESERVATIONS[date]
+	return f"No Reservation at {date}"
 
-app.run("0.0.0.0",8080,debug=True)
+app.run("0.0.0.0",8080)
